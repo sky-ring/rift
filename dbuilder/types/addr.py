a8bf19b2f54bde2b9db0b81e73f03ddfe15de93f
Amin Rezaei <AminRezaei0x443@gmail.com>
2022-08-24 17:18:27 +0430

    feature: partial impl coin and addr types

---
+++ dbuilder/types/addr.py
@@ -0,0 +1,19 @@
+from dbuilder.core import Entity
+from dbuilder.types.types import Builder, Int, Slice
+
+
+class MsgAddress(Slice):
+    @classmethod
+    def __serialize__(cls, to: "Builder", value: "Entity") -> "Builder":
+        b = to.slice(value)
+        return b
+
+    @classmethod
+    def __deserialize__(
+        cls, from_: "Slice", name: str = None, inplace: bool = True,
+    ):
+        # TODO: HANDLE INPLACE STUFF
+        v = from_.addr_()
+        if name is not None:
+            v.__assign__(name)
+        return v
---
+++ dbuilder/types/coin.py
@@ -0,0 +1,19 @@
+from dbuilder.core import Entity
+from dbuilder.types.types import Builder, Int, Slice
+
+
+class Coin(Int):
+    @classmethod
+    def __serialize__(cls, to: "Builder", value: "Entity") -> "Builder":
+        b = to.coins(value)
+        return b
+
+    @classmethod
+    def __deserialize__(
+        cls, from_: "Slice", name: str = None, inplace: bool = True,
+    ):
+        # TODO: HANDLE INPLACE STUFF
+        v = from_.coin()
+        if name is not None:
+            v.__assign__(name)
+        return v
