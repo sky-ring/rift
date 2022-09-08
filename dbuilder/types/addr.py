ac642fe9724acf7427b6108b8892b8a3f38dcd71
Amin Rezaei <AminRezaei0x443@gmail.com>
2022-09-08 20:36:34 +0430

    feature: std addr build

--- dbuilder/types/addr.py
+++ dbuilder/types/addr.py
@@ -1,10 +1,18 @@
 from dbuilder.core import Entity
-from dbuilder.types.int_aliases import uint2
-from dbuilder.types.types import Builder, Int, Slice
+from dbuilder.types.int_aliases import uint2, int8, uint256
+from dbuilder.types.types import Builder, Cell, Int, Slice
+from dbuilder.types.maybe import Maybe
+from dbuilder.types.payload import Payload
 
 
 class MsgAddress(Slice):
     Empty = uint2(0b00)
+
+    class Std(Payload):
+        __tag__ = "10"
+        anycast: Maybe[Cell]
+        workchain: int8
+        address: uint256
 
     @classmethod
     def __serialize__(cls, to: "Builder", value: "Entity") -> "Builder":
@@ -28,3 +36,11 @@ class MsgAddress(Slice):
         if name is not None:
             v.__assign__(name)
         return v
+
+    @classmethod
+    def std(cls, workchain: int, addr: uint256) -> Slice:
+        return cls.Std(
+            anycast=None,
+            workchain=workchain,
+            address=addr,
+        ).as_cell().parse()
