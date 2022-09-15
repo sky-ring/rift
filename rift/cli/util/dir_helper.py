a7411853dbdb5fa511aee726d5f6da4899a6f639
Amin Rezaei <AminRezaei0x443@gmail.com>
2022-09-15 13:10:20 +0430

    fix: makedirs call

--- rift/cli/util/dir_helper.py
+++ rift/cli/util/dir_helper.py
@@ -49,7 +49,7 @@ class DirectoryStructure(_DirNode):
             p = leaf.as_dir()
             try:
                 _prev_umask = umask(0)
-                makedirs(p, exists_ok, mode=0o777)
+                makedirs(p, exists_ok, 0o777)
             except Exception as e:
                 print(e)
                 traceback.print_exc()
