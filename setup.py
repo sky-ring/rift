f3e53adf54a57cfaf8cdd6e3398783ad9d9291b4
Amin Rezaei <AminRezaei0x443@gmail.com>
2023-02-22 14:14:04 +0330

    release: v0.10.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rift-framework",
-    version="0.9.14",
+    version="0.10.0",
     description="A magical Python3 -> TON portal",
     license="MIT",
     packages=find_packages(exclude=["test"]),
